396c781d1ede35a488eee7dca5db3da7322245d7
KateBin™ <72105928+Katebin@users.noreply.github.com>
2022-10-31 11:25:32 -0400

    Fixed Motor Ports 🦼
        public static final int LEFT_MOTOR_PORT_A = 4;
        public static final int LEFT_MOTOR_PORT_B = 5;
        public static final int LEFT_MOTOR_PORT_C = 6;
    
        // right motors
        public static final int RIGHT_MOTOR_PORT_A = 1;
        public static final int RIGHT_MOTOR_PORT_B = 2;
        public static final int RIGHT_MOTOR_PORT_C = 3;

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,14 +5,14 @@ public class Constants {
     public static final int JOYSTICK_PORT = 0;
 
     // left motors
-    public static final int LEFT_MOTOR_PORT_A = 1;
-    public static final int LEFT_MOTOR_PORT_B = 2;
-    public static final int LEFT_MOTOR_PORT_C = 3;
+    public static final int LEFT_MOTOR_PORT_A = 4;
+    public static final int LEFT_MOTOR_PORT_B = 5;
+    public static final int LEFT_MOTOR_PORT_C = 6;
 
     // right motors
-    public static final int RIGHT_MOTOR_PORT_A = 4;
-    public static final int RIGHT_MOTOR_PORT_B = 5;
-    public static final int RIGHT_MOTOR_PORT_C = 6;
+    public static final int RIGHT_MOTOR_PORT_A = 1;
+    public static final int RIGHT_MOTOR_PORT_B = 2;
+    public static final int RIGHT_MOTOR_PORT_C = 3;
 
     // drive
     public static final double WHEEL_BASE_M = 0.5; // distance between left / right wheels in meters
