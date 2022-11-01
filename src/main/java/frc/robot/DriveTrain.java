2c13c41626f5a57cf5b16e88ca6f891c1ae4dc26
KateBin™ <programming.kaitlyn.williams@gmail.com>
2022-11-01 16:15:31 -0400

    Arcade fix 2.1

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -15,25 +15,46 @@ public class DriveTrain {
         this.controller = controller;
     }
 
+    private boolean isStill(double value) {
+        // returns bool if joystick is not moving
+        // basic but needed to save me time & fix joystick
+        
+        if(value > 0.15) {
+            return false;
+        } else if(value < -0.15) {
+            return false;
+        } else if(value == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     public void move() {
         // method moves robot according to user input
         double speed = controller.getLeftY();
         double bias = controller.getRightX();
 
         // joystick controlls
-        if(speed == 0 && bias > 0) {
+        if(isStill(speed) == true && bias > 0.1) {
             // static turn right
             motorLeft.safeSet(bias);
             motorRight.safeSet(bias * -1);
 
-        } else if(speed == 0 && bias < 0) {
+            System.out.println("static right");
+
+        } else if(isStill(speed) == true && bias < -0.1) {
             // static turn left
             motorLeft.safeSet(bias * -1);
             motorRight.safeSet(bias);
 
-        } else if(speed != 0 && bias > 0) {
+            System.out.println("static left");
+
+        } else if(isStill(speed) != true && bias > 0.1) {
             // dynamic turn right
             motorLeft.safeSet(speed);
+
+            System.out.println("dynamic right");
 
             // ensure that robot wont perform a dangerous static turn while at speed
             if(speed - bias < 0) {
@@ -41,9 +62,11 @@ public class DriveTrain {
             } else {
                 motorRight.safeSet(speed - bias);
             }
-        } else if(speed != 0 && bias < 0) {
+        } else if(isStill(speed) != true && bias < -0.1) {
             // dynamic turn left
             motorLeft.safeSet(speed);
+
+            System.out.println("dynamic left");
 
             // ensure that robot wont perform a dangerous static turn while at speed
             if(speed - (bias * -1) < 0) {
@@ -51,15 +74,19 @@ public class DriveTrain {
             } else {
                 motorLeft.safeSet(speed - (bias * -1));
             }
-        } else if(speed != 0) {
+        } else if(isStill(speed) != true) {
             // vertical movement
             motorLeft.safeSet(speed);
             motorRight.safeSet(speed);
+
+            System.out.println("moving");
 
         } else {
             // no input
             motorLeft.safeSet(0);
             motorRight.safeSet(0);
+
+            System.out.println("still");
         }
     }
 }
